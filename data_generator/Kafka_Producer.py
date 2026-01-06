1b5b265572b2af160c7566cfa77c7d1b1c69c215
EvilGit84 <vishalmundhe000@gmail.com>
2026-01-06 20:45:33 +0530

    Add Power BI dashboard and analytics views

--- data_generator/Kafka_Producer.py
+++ data_generator/Kafka_Producer.py
@@ -1,18 +1,29 @@
 from kafka import KafkaProducer
 import json
 import time
+import random
+
 
 producer = KafkaProducer(
     bootstrap_servers='127.0.0.1:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-orders =[
-    {'order_id': 101, 'amount': 300, 'city': 'pune'},
-    {'order_id': 102, 'amount': 150, 'city': 'mumbai'}
-]
-for order in orders:
-    producer.send('retail_orders', order)
-    print(f"Sent order: {order}")
-    time.sleep(1)
+cities = ['pune', 'mumbai', 'bangalore', 'delhi', 'hyderabad']
+TOTAL_RECORDS = 100000
+print(f"Producing {TOTAL_RECORDS} messages to Kafka topic 'retail_orders'...")
+
+for i in range(1, TOTAL_RECORDS + 1):
+    order = {
+        'order_id': i,
+        'amount': random.randint(100, 10000),
+        'city': random.choice(cities)   
+    }
+
+    producer.send('retail_orders', value=order)
+    if i % 1000 == 0:
+        print(f"Sent {i} messages so far...")    
+        time.sleep(0.01)  # Simulate some delay
+
 producer.flush()
+print("Finished producing records.") 
